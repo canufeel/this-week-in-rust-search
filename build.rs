a871be0b0c289545f83d544ee2a893e98f15a4ad
Petr Kosikhin <petr.kosikhin@gmail.com>
2020-04-11 23:04:00 -0700

    fix build script to install yarn if it is missing

--- build.rs
+++ build.rs
@@ -1,6 +1,13 @@
-use std::process::Command;
+use std::process::{Command};
 
 fn main() {
+  if !Command::new("npm")
+    .args(&["view", "yarn", "version", "--global"])
+    .status()
+    .unwrap()
+    .success() {
+    Command::new("npm").args(&["install", "-g", "yarn"]).status().unwrap();
+  }
   Command::new("yarn").status().unwrap();
   Command::new("yarn").args(&["build", "--production"]).status().unwrap();
 }
